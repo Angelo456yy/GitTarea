edd75e94ea7f9200660c0544c8832dd9baa03e8d
Angelo456yy <alopezm114@miumg.edu.gt>
2025-08-01 14:40:49 -0600

    v4

--- Tarea.py
+++ Tarea.py
@@ -4,7 +4,19 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "No se puede dividir por cero"
+
 a = int(input("Ingrese el primer número: "))
 b = int(input("Ingrese el segundo número: "))
-print("La suma es:", sumar(a, b))
-print("La resta es:", restar(a, b))
+
+print("Suma:", sumar(a, b))
+print("Resta:", restar(a, b))
+print("Multiplicación:", multiplicar(a, b))
+print("División:", dividir(a, b))
