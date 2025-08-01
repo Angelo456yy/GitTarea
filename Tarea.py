f7b6b61c575c2e1babe4f92578304b5cf10f25e6
Angelo456yy <alopezm114@miumg.edu.gt>
2025-08-01 14:39:59 -0600

    v3

--- Tarea.py
+++ Tarea.py
@@ -1,4 +1,10 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
 a = int(input("Ingrese el primer número: "))
 b = int(input("Ingrese el segundo número: "))
-print("La suma es:", a + b)
-print("La resta es:", a - b)
+print("La suma es:", sumar(a, b))
+print("La resta es:", restar(a, b))
